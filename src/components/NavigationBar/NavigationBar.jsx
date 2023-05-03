d2ff1a584715a7255d9a1f92dfaa7e5f29a825e7
Partho Roy <roypartho772@gmail.com>
2023-05-04 02:05:38 +0600

    navbar added

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -6,17 +6,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 const NavigationBar = () => {
     return (
-        <div className=''>
-            <Navbar collapseOnSelect expand="lg" bg='light' variant='light'>
-                <Container className='py-2'>
-                    <Link className='text-primary text-decoration-none fw-bold fs-4' to="/">The Continental Kitchen</Link>
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg='warning' variant='light'>
+                <Container className='py-2 '>
+                    <Link className='text-black fst-italic text-decoration-none fw-bold fs-4' to="/">The Continental Kitchen</Link>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto d-flex align-items-center">
-                            <Link className='me-3 text-decoration-none fw-semibold' to="/">Home</Link>
-                            <Link className='me-3 text-decoration-none fw-semibold' to="/blog">Blogs</Link>
-                            <Link className='me-3 text-decoration-none fw-semibold' to="/login">Login</Link>
-                            <Link className='me-3 text-decoration-none fw-semibold' to="/register">Register</Link>
+                            <Link className='me-4 text-secondary text-decoration-none fw-semibold' to="/">Home</Link>
+                            <Link className='me-4 text-secondary text-decoration-none fw-semibold' to="/blog">Blogs</Link>
+                            <Link className='me-4 text-secondary text-decoration-none fw-semibold' to="/login">Login</Link>
+                            <Link className='text-secondary text-decoration-none fw-semibold' to="/register">Register</Link>
                         </Nav>
                         <Nav>
                             
